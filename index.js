10bcc48e7b5246d78bba77c7965d6a8848d7033c
Rhaigtz <nopazog@gmail.com>
2020-06-08 19:34:48 -0400

    solucionando bloqueo

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-'use strict'
-let scholar = require('google-scholar-extended')
+scholar = require('./src/utils/scholar')
+let cheerioObject = require("fetch-cheerio-object");
 
-scholar.profile('PL1YL-MAAAAJ')
-  .then(resultsObj => {
-    console.log(resultsObj)
-  })
+
+function loadCheerio(html) {
+      cheerioObject(html).then(($) => {
+          console.log($)
+        const websiteTitle = $(".gsc_vcd_title_link");
+        console.log("Title: ", websiteTitle.text());
+      });
+}
+
+
+(async () => {
+    console.log('hola')
+    const $ = await cheerioObject('https://scholar.google.cl/citations?view_op=view_citation&hl=es&user=PL1YL-MAAAAJ&citation_for_view=PL1YL-MAAAAJ:d1gkVwhDpl0C');
+    console.log($('.gsc_vcd_title a').html()); 
+  })();
+//loadCheerio("https://scholar.google.cl/citations?view_op=view_citation&hl=es&user=PL1YL-MAAAAJ&citation_for_view=PL1YL-MAAAAJ:d1gkVwhDpl0C");
