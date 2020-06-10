07c3fb8f5342d83177efb7858c89accc8e527fe2
Nicolás Opazo <nopazog@gmail.com>
2020-06-09 23:45:01 -0400

    UPDATE: Se termina implementacion script schoolar

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-scholar = require('./src/utils/scholar')
-let cheerioObject = require("fetch-cheerio-object");
+scholar = require('./src/utils/scholar');
+let cheerioObject = require('fetch-cheerio-object');
 
+const loadCheerio = async (html, beforeData) => {
+  const $ = await cheerioObject(html);
+  let year = '';
+  let book = '';
+  let conference = '';
+  let journal = '';
+  let pages = '';
+  let volume = '';
+  let issue = '';
+  let autors = '';
+  $('.gs_scl').each((i, r) => {
+    if ($(r).find('.gsc_vcd_field').text() === 'Fecha de publicación') {
+      year = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Libro') {
+      book = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Revista') {
+      journal = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Conferencia') {
+      conference = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Volumen') {
+      volume = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Páginas') {
+      pages = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Número') {
+      issue = $(r).find('.gsc_vcd_value').text();
+    }
+    if ($(r).find('.gsc_vcd_field').text() === 'Autores') {
+      autors = $(r).find('.gsc_vcd_value').text();
+    }
+  });
+  return {
+    ...beforeData,
+    year,
+    book,
+    journal,
+    conference,
+    pages,
+    issue,
+    autors,
+  };
+};
 
-function loadCheerio(html) {
-      cheerioObject(html).then(($) => {
-          console.log($)
-        const websiteTitle = $(".gsc_vcd_title_link");
-        console.log("Title: ", websiteTitle.text());
-      });
-}
-
-
-(async () => {
-    console.log('hola')
-    const $ = await cheerioObject('https://scholar.google.cl/citations?view_op=view_citation&hl=es&user=PL1YL-MAAAAJ&citation_for_view=PL1YL-MAAAAJ:d1gkVwhDpl0C');
-    console.log($('.gsc_vcd_title a').html()); 
-  })();
-//loadCheerio("https://scholar.google.cl/citations?view_op=view_citation&hl=es&user=PL1YL-MAAAAJ&citation_for_view=PL1YL-MAAAAJ:d1gkVwhDpl0C");
+scholar
+  .profile('PL1YL-MAAAAJ')
+  .then((response) => {
+    console.log(response.results);
+    return response.results;
+  })
+  .then((listData) => {
+    Promise.all(
+      listData.map((item) => {
+        console.log('item ', item);
+        return loadCheerio(item.relatedUrl, item);
+      })
+    ).then((response) => console.log('finalResponse', response));
+  });
--- src/utils/scholar.js
+++ src/utils/scholar.js
@@ -1,51 +1,33 @@
-"use strict";
-let fetch = require("node-fetch");
-let request = require("request");
-let requeest = require("request-promise");
-let cheerio = require("cheerio");
-let striptags = require("striptags");
-let cheerioObject = require("fetch-cheerio-object");
-const throttledQueue = require("throttled-queue");
+'use strict';
+let request = require('request');
+let cheerio = require('cheerio');
+const throttledQueue = require('throttled-queue');
 
-function loadCheerio(html) {
-    perSecThrottle(() => {
-      fetchCheerioObject("https://example.org/").then(($) => {
-        const websiteTitle = $(".gsc_vcd_title a");
-        console.log("Title: ", websiteTitle.html());
-      });
-    });
-}
 let scholar = (function () {
   // 1 per 200 ms ~= 5/s per
   // https://developers.google.com/webmaster-tools/search-console-api-original/v3/limits
 
   const perSecThrottle = throttledQueue(1, 5000);
   const perMinThrottle = throttledQueue(200, 60 * 1000);
-  const RESULTS_PER_PAGE = 10;
-
-  const GOOGLE_SCHOLAR_URL = "https://scholar.google.com/scholar?hl=en&q=";
   const GOOGLE_SCHOLAR_PROFILE_URL =
-    "https://scholar.google.co.uk/citations?hl=en&user=";
-  const GOOGLE_SCHOLAR_URL_PREFIX = "https://scholar.google.com";
+    'https://scholar.google.cl/citations?hl=es&user=';
+  const GOOGLE_SCHOLAR_URL_PREFIX = 'https://scholar.google.cl';
 
-  const ELLIPSIS = "...";
-  const ELLIPSIS_HTML_ENTITY = "&#x2026;";
-  const COMMA = ",";
-  const COMMA_HTML_ENTITY = "&#xFFFD;";
-  const ET_AL_NAME = "et al.";
-  const CITATION_COUNT_PREFIX = "Cited by ";
-  const RELATED_ARTICLES_PREFIX = "Related articles";
+  const ELLIPSIS = '...';
+  const ELLIPSIS_HTML_ENTITY = '&#x2026;';
+  const COMMA = ',';
+  const COMMA_HTML_ENTITY = '&#xFFFD;';
+  const CITATION_COUNT_PREFIX = 'Cited by ';
 
   const STATUS_CODE_FOR_RATE_LIMIT = 503;
-  const STATUS_MESSAGE_FOR_RATE_LIMIT = "Service Unavailable";
+  const STATUS_MESSAGE_FOR_RATE_LIMIT = 'Service Unavailable';
   const STATUS_MESSAGE_BODY =
     'This page appears when Google automatically detects requests coming from your computer network which appear to be in violation of the <a href="//www.google.com/policies/terms/">Terms of Service</a>. The block will expire shortly after those requests stop.';
 
   // regex with thanks to http://stackoverflow.com/a/5917250/1449799
   const RESULT_COUNT_RE = /\W*((\d+|\d{1,3}(,\d{3})*)(\.\d+)?) results/;
 
-
-  function responseAction(reject,error, response, html, callback) {
+  function responseAction(error, response, html, callback) {
     if (error) {
       Promise.reject(error);
     } else if (response.statusCode !== 200) {
@@ -54,17 +36,15 @@ let scholar = (function () {
         response.statusMessage === STATUS_MESSAGE_FOR_RATE_LIMIT &&
         response.body.indexOf(STATUS_MESSAGE_BODY) > -1
       ) {
-          reject('mala tu wea')
         Promise.reject(
           new Error(
-            "you are being rate-limited by google. you have made too many requests too quickly. see: https://support.google.com/websearch/answer/86640"
+            'you are being rate-limited by google. you have made too many requests too quickly. see: https://support.google.com/websearch/answer/86640'
           )
         );
       } else {
-          reject('mala tu wea')
         Promise.reject(
           new Error(
-            "expected statusCode 200 on http response, but got: " +
+            'expected statusCode 200 on http response, but got: ' +
               response.statusCode
           )
         );
@@ -82,14 +62,6 @@ let scholar = (function () {
     }
   }
 
-  function removeFromBeginning(string, remove, callback) {
-    if (string.substr(0, remove.length) === remove) {
-      callback(true, string.substr(remove.length + 2));
-    } else {
-      callback(false, string);
-    }
-  }
-
   function scholarResultsCallback(
     resolve,
     reject,
@@ -100,53 +72,27 @@ let scholar = (function () {
     FOOTER_LINKS_TAG
   ) {
     return (error, response, html) => {
-      responseAction(reject ,error, response, html, (html) => {
+      responseAction(error, response, html, (html) => {
         let $ = cheerio.load(html);
 
         let results = $(RESULTS_TAG);
         let resultCount = 0;
-
+        let counter = 0;
         let processedResults = [];
         results.each((i, r) => {
           let title = $(r).find(TITLE_TAG).text().trim();
-          let url = $(r).find(URL_TAG).attr("href");
-          let authorNamesHTMLString = $(r).find(AUTHOR_NAMES_TAG).html();
-          let etAl = false;
-          let etAlBegin = false;
+          let url = $(r).find(URL_TAG).attr('href');
           let authors = [];
           let footerLinks = $(r).find(FOOTER_LINKS_TAG);
           let citedCount = 0;
           let relatedUrl =
-            GOOGLE_SCHOLAR_URL_PREFIX + $(r).find(TITLE_TAG).attr("data-href");
-          let pdfUrl = $($(r).find(".gs_ggsd a")[0]).attr("href");
-          let year = "";
-          let book = "";
-          let conference = "";
-          let journal = "";
-          let pages = "";
-          let volume = "";
-          let issue = "";
-
+            GOOGLE_SCHOLAR_URL_PREFIX +
+            $(r).find(TITLE_TAG).attr('data-href').trim();
+          let pdfUrl = $($(r).find('.gs_ggsd a')[0]).attr('href');
           // Profile specific
-          $(r).find(".gs_gray").last().find(".gs_oph").remove();
-          let venueHTMLString = $(r).find(".gs_gray").last().html();
+          $(r).find('.gs_gray').last().find('.gs_oph').remove();
+          let venueHTMLString = $(r).find('.gs_gray').last().html();
           let venue;
-
-          if (relatedUrl) {
-           // loadCheerio(relatedUrl);
-
-            let temporal = cheerio.load(relatedUrl.toString());
-            let temporal_results = temporal(".gsc_vcd_table");
-            temporal_results.each((i, r) => {
-              console.log("hola");
-              if (
-                temporal_results(".gs_scl .gsc_vcd_field").text() ===
-                "Fecha de publicación"
-              ) {
-                year = "hola";
-              }
-            });
-          }
           if ($(footerLinks[0]).text().indexOf(CITATION_COUNT_PREFIX) >= 0) {
             citedCount = $(footerLinks[0])
               .text()
@@ -165,70 +111,6 @@ let scholar = (function () {
                 .substr(CITATION_COUNT_PREFIX.length);
             }
           }
-          if (authorNamesHTMLString) {
-            let cleanString = authorNamesHTMLString;
-
-            // Check also for non-HTML ellipsis.
-            removeFromEnd(
-              cleanString,
-              ELLIPSIS_HTML_ENTITY,
-              (resultA, stringA) => {
-                removeFromEnd(cleanString, ELLIPSIS, (resultB, stringB) => {
-                  if (resultA) {
-                    cleanString = stringA;
-                  } else if (resultB) {
-                    cleanString = stringB;
-                  } else if (resultA || resultB) {
-                    etAl = true;
-                  }
-                });
-              }
-            );
-
-            removeFromBeginning(
-              cleanString,
-              ELLIPSIS_HTML_ENTITY,
-              (resultA, stringA) => {
-                removeFromBeginning(
-                  cleanString,
-                  ELLIPSIS,
-                  (resultB, stringB) => {
-                    if (resultA) {
-                      cleanString = stringA;
-                    } else if (resultB) {
-                      cleanString = stringB;
-                    } else if (resultA || resultB) {
-                      etAlBegin = true;
-                    }
-                  }
-                );
-              }
-            );
-
-            let htmlAuthorNames = cleanString.split(", ");
-            if (etAl) {
-              htmlAuthorNames.push(ET_AL_NAME);
-            }
-            if (etAlBegin) {
-              htmlAuthorNames.unshift(ET_AL_NAME);
-            }
-            authors = htmlAuthorNames.map((name) => {
-              let tmp = cheerio.load(name);
-              let authorObj = {
-                name: "",
-                url: "",
-              };
-              if (tmp("a").length === 0) {
-                authorObj.name = striptags(name);
-              } else {
-                authorObj.name = tmp("a").text();
-                authorObj.url =
-                  GOOGLE_SCHOLAR_URL_PREFIX + tmp("a").attr("href");
-              }
-              return authorObj;
-            });
-          }
-
           // Profile specific.
           if (venueHTMLString) {
             venue = venueHTMLString;
@@ -266,26 +148,26 @@ let scholar = (function () {
             citedCount: citedCount,
             relatedUrl: relatedUrl,
             pdf: pdfUrl,
-            year: year,
             venue: venue,
+            extraValues: extraValues,
           });
         });
 
-        let resultsCountString = $("#gs_ab_md").text();
+        let resultsCountString = $('#gs_ab_md').text();
         if (resultsCountString && resultsCountString.trim().length > 0) {
           let matches = RESULT_COUNT_RE.exec(resultsCountString);
           if (matches && matches.length > 0) {
-            resultCount = parseInt(matches[1].replace(/,/g, ""));
+            resultCount = parseInt(matches[1].replace(/,/g, ''));
           } else {
             resultCount = processedResults.length;
           }
         } else {
           resultCount = processedResults.length;
         }
-        reject()
         resolve({
           results: processedResults,
           count: resultCount,
+          counter: counter,
         });
       });
     };
@@ -293,22 +175,28 @@ let scholar = (function () {
 
   function profile(id) {
     let p = new Promise(function (resolve, reject) {
-      var requestOptions = {
-        jar: true,
-      };
-      requestOptions.url = encodeURI(GOOGLE_SCHOLAR_PROFILE_URL + id);
-      request(
-        requestOptions,
-        scholarResultsCallback(
-          resolve,
-          reject,
-          ".gsc_a_tr",
-          ".gsc_a_t a",
-          ".gs_ri h3 a",
-          ".gs_gray",
-          ".gsc_a_c a"
-        )
-      );
+      perMinThrottle(() => {
+        perSecThrottle(() => {
+          var requestOptions = {
+            jar: true,
+          };
+          requestOptions.url = encodeURI(
+            GOOGLE_SCHOLAR_PROFILE_URL + id + '&cstart=0&pagesize=51'
+          );
+          request(
+            requestOptions,
+            scholarResultsCallback(
+              resolve,
+              reject,
+              '.gsc_a_tr',
+              '.gsc_a_t a',
+              '.gs_ri h3 a',
+              '.gs_gray',
+              '.gsc_a_c a'
+            )
+          );
+        });
+      });
     });
     return p;
   }
